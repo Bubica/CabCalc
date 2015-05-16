9c17600ebdd26a338ca96658cc0ecbbe8f704b71
Bubica <bubica@AgatasMac.local>
2015-05-16 16:18:47 +0100

    py/sql code modifications

---
+++ sql_cabcalc/manhattan.sh
@@ -0,0 +1,16 @@
+#Load the basic setup
+source db_setup.sh
+
+#Add extra boolean column
+for TRIP_ID in `seq 3 12`;
+do
+	TRIP_TABLE="trip_"$TRIP_ID
+
+    #Create extra columns in trip table (if not already present)
+    SQL_CMD="ALTER TABLE "$TRIP_TABLE" ADD COLUMN manhattan TINYINT(1)"
+    echo $SQL_CMD
+    mysql -e "$SQL_CMD" -u root  $DB_NAME
+
+    # python -m $PY_MODULE $TRIP_ID manhattan
+    
+done
